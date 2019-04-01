8dece63bb3e7db38a3f88cba84e369b863ddcd16
Unknown <toutlemonde74@gmail.com>
2019-04-01 19:45:27 +0200

    wip  basics vue component menu

---
+++ src/components/Accueil.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="accueil">
+        <section class="menu">
+            <h1>Nos Menus</h1>
+            <article>
+                <menu-card name="nom du menu 1"></menu-card>
+                <menu-card name="nom du menu 2"></menu-card>
+            </article>
+            <article>
+                <menu-card name="nom du menu 3"></menu-card>
+                <menu-card name="nom du menu 4"></menu-card>
+            </article>
+
+        </section>
+        <section class="carte">
+            <h1>Menu à la Carte</h1>
+        </section>
+    </div>
+</template>
+
+<script>
+  import MenuCard from '@/components/MenuCard'
+  import AddPlat from '@/components/AddPlat'
+
+  export default {
+    name: 'Accueil',
+    components: {
+      MenuCard,
+      AddPlat
+    },
+    data () {
+      return {
+        'test': 'test'
+      }
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+    .accueil {
+
+    }
+</style>
---
+++ src/components/MenuCard.vue
@@ -0,0 +1,25 @@
+<template>
+    <aside><p>{{img}}</p><p>{{name}}</p></aside>
+</template>
+
+<script>
+  export default {
+    name: 'MenuCart',
+    props: {
+      name: {
+        type: [Number, String],
+        default: 0
+      }
+    },
+    data () {
+      return {
+        'img': 'img'
+      }
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+
+
+</style>
