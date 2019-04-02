252b17f1896ec8d4ae585006ce2ce18b195d7ce8
Alex-Chopard <alex.chopard1998@gmail.com>
2019-04-02 11:50:27 +0200

    Add endpoint

--- src/services/endpoints.js
+++ src/services/endpoints.js
@@ -7,6 +7,14 @@ export default {
     list: { method: 'GET', url: 'api/plat' },
     get: { method: 'GET', url: 'api/plat/:id' }
   },
+  exploitation: {
+    list: { method: 'GET', url: 'api/exploitation' },
+    get: { method: 'GET', url: 'api/exploitation/:id' }
+  },
+  ingredient: {
+    list: { method: 'GET', url: 'api/ingredient' },
+    get: { method: 'GET', url: 'api/ingredient/:id' }
+  },
   typePlat: {
     list: { method: 'GET', url: 'api/typePlat' }
   }
