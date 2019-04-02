3aa2fe22a8c17da60402540478d410519b4ba66b
StephLaudou <stephanie.laudou@gmail.com>
2019-04-02 14:58:22 +0200

    recapitulatif bouton suppr ligne

--- src/components/Recapitulatif.vue
+++ src/components/Recapitulatif.vue
@@ -2,9 +2,9 @@
   <div>
     <h1>Recapitulatif de la commande</h1>
     <button @click="back">Back</button>
-    <ul>   
-    <li v-for='item in items'>
-        {{item.name}} {{item.quantity}}  <button @click="deleteDish">Supprimer</button>
+    <ul class="liste">   
+    <li class="item" v-for='item in items'>
+        {{item.name}} <span>Quantité :  {{item.quantity}} </span> <button @click="deleteDish(item.id)">Supprimer</button>
     </li>    
     </ul>
     <button @click="validateOrder">Valider la commande</button>
@@ -26,8 +26,8 @@ export default {
   data () {
     return {
       items:
-      [{name: 'Quinoa', quantity: '2', amount: '0'},
-      {name: 'Riz', quantity: '3', amount: '0'}]
+      [{name: 'Quinoa', quantity: '2', amount: '0', id: 0},
+      {name: 'Riz', quantity: '3', amount: '0', id: 1}]
     }
   },
   watch: {
@@ -39,8 +39,9 @@ export default {
     validateOrder () {
       console.log('order validated')
     },
-    deleteDish () {
+    deleteDish (id) {
       console.log('dish deleted')
+      console.log(id)
     },
     deleteOrder () {
       console.log('orderdeteled : todo lien vers le store')
@@ -51,11 +52,20 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 
 h1 {
   color: green;
   text-align: center;
+}
+.liste{
+  margin: 20px;
+}
+.item {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  font-size: 30px;
 }
 
 </style>
