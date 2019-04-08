69580753d01eda3d30ae5a2b69b94b8c9ea0df45
Maxime Girard <mx.girard42@gmail.com>
2019-04-08 15:53:50 +0200

    mise en place fonctionnel popup par plat

--- src/components/Listing.vue
+++ src/components/Listing.vue
@@ -6,14 +6,27 @@
         <div class="listing-choice">
             <section class="choice">
                 <aside v-for="plat in plats">
-                    <article class="choice-detail" @click="onItemClick(plat.id)">
+                    <article class="choice-detail" @click="onItemClick(plat)">
                         <img v-bind:src="plat.urlPhoto" alt="">
                         <p>{{ plat.nom }}</p>
                     </article>
                 </aside>
             </section>
         </div>
-        <pop-up typePopUp="Plat" v-if="showPopUpPlat" @close="showPopUpPlat = false"></pop-up>
+        <pop-up typePopUp="Plat" v-if="showPopUpPlat" @close="showPopUpPlat = false" class="pop-up">
+            <div v-if="currentPlat.urlPhoto != null">
+                <div class="photo">
+                    <div>
+                        <img v-bind:src="currentPlat.urlPhoto" v-bind:alt="currentPlat.nom">
+                    </div>
+                </div>
+                <div class="description">
+                    <h2>{{currentPlat.nom}}</h2>
+                    <p>{{currentPlat.description}}</p>
+                    <p>{{currentPlat.prix}} €</p>
+                </div>
+            </div>
+        </pop-up>
     </div>
 </template>
 
@@ -38,9 +51,11 @@
       returnBack () {
         this.$router.go(-1)
       },
-      onItemClick (id) {
-        console.log('http://localhost:8000/#/listing/' + id)
+      async onItemClick (plat) {
+        console.log(plat)
         this.showPopUpPlat = true
+        this.currentPlat = plat
+        this.currentPlat = (await fetch(endpoints.plat.get, {id: plat.id})).data
         // this.$router.go('http://localhost:8000/#/listing/' + id)
       },
       async getPlats () {
@@ -53,7 +68,8 @@
     data () {
       return {
         plats: [],
-        'showPopUpPlat': false
+        'showPopUpPlat': false,
+        currentPlat: null
       }
     }
   }
@@ -62,12 +78,10 @@
 <style scoped lang="scss">
     .listing {
         display: flex;
-
         &-choice {
             height: 100vh;
             width: 100%;
             text-align: center;
-
             .choice {
                 display: flex;
                 flex-direction: row;
@@ -76,10 +90,8 @@
                 height: calc(100vh - 112px);
                 margin-top: 112px;
                 flex-wrap: wrap;
-
                 aside {
                     width: 33%;
-
                     article {
                         img {
                             height: 170px;
@@ -87,6 +99,40 @@
                             box-sizing: border-box;
                             box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
                         }
+                    }
+                }
+            }
+        }
+        .pop-up {
+            div {
+                display: flex;
+                flex-direction: row;
+                height: 100%;
+                .photo {
+                    width: 44%;
+                    height: 100%;
+                    border-bottom-left-radius: 20px;
+                    border-top-left-radius: 20px;
+                    display: flex;
+                    div {
+                        border-top-left-radius: 20px;
+                        border-bottom-left-radius: 20px;
+                        max-width: 100%;
+                        overflow: hidden;
+                        img {
+                            position: relative;
+                            transform: translate(-50%,-50%) scale(1.01);
+                            top: 50%;
+                            left: 50%;
+                        }
+                    }
+                }
+                .description {
+                    width: 66%;
+                    display:flex;
+                    flex-direction: column;
+                    * {
+                        margin: 10px 20px;
                     }
                 }
             }
