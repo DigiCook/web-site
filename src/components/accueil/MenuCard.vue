8ca60c283af342417c179eab0fee358213b956e2
Alex-Chopard <alex.chopard1998@gmail.com>
2019-04-29 14:46:53 +0200

    Use previous css of card

---
+++ src/components/accueil/MenuCard.vue
@@ -0,0 +1,46 @@
+<template>
+  <aside v-on:click="onClick(dataMenu.id)" class="unMenu">
+    <img class="unMenu-img" :src="dataMenu.urlPhoto"/>
+    <p class="unMenu-text">{{dataMenu.nom}}</p>
+  </aside>
+</template>
+
+<script>
+export default {
+  name: 'MenuCart',
+  props: {
+    dataMenu: {
+      type: [Object]
+    }
+  },
+  methods: {
+    onClick (id) {
+      this.$router.push(`menu/${id}`)
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+.unMenu {
+  margin: 20px;
+  box-shadow: -2px 0px 10px 1px rgba(0, 0, 0, 0.15);
+  border-radius: $card-border-radius;
+  padding: 20px;
+
+  &-img {
+    width: 22vw;
+    border-radius: 50%;
+  }
+
+  &-text {
+    font-size: $sub-title;
+    text-align: center;
+    padding-top: 4px;
+    color: $text-dark-ligth;
+    font-weight: $weight-sub-title;
+  }
+}
+
+</style>
