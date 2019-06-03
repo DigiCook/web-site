e3fd2c100b5da4949872e7fccc5c65c66df4030a
Alex-Chopard <alex.chopard1998@gmail.com>
2019-06-03 15:46:12 +0200

    Add Live omponent

---
+++ src/components/Live.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="Live">
+    <video src="/vdo.webm" autoplay="true" width="640" height="480" preload="metadata"/>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Live',
+  components: {
+  },
+  props: {
+  },
+  mounted () {
+    console.log('[Live:mounted]')
+  },
+  data () {
+    return {
+    }
+  },
+  watch: {
+  },
+  methods: {
+  },
+  computed: {
+  }
+}
+</script>
+
+
+<style scoped lang="scss">
+</style>
