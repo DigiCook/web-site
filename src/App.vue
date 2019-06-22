e83ad376c43d693ed4a7afd5a587366f4cd9e412
Quidam74 <toutlemonde74@gmail.com>
2019-06-22 16:04:08 +0200

    overlay tab ok

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,14 @@
 <template>
+
     <div class="tablette-inner">
-        <div class="maxHeight">
-            <router-view/>
-            <snackbar></snackbar>
+        <div class="tablette">
+            <div class="camm"></div>
+            <div class="size">
+                <router-view/>
+                <snackbar></snackbar>
+            </div>
+            <div class="btnHome"></div>
+
         </div>
     </div>
 </template>
@@ -28,14 +34,43 @@
 </style>
 
 <style scoped>
-    .tablette-inner{
+    .tablette-inner {
         display: flex;
         height: 100vh;
     }
-    .maxHeight {
-        margin: auto;
+
+    .camm {
+        width: 10px;
+        height: 10px;
+        background-color: #666;
+        border-radius: 50%;
+    }
+
+    .btnHome {
+        width: 35px;
+        height: 35px;
+        background-color: #EEE;
+        border: solid 2px #AAA;
+        border-radius: 50%;
+    }
+
+    .size {
         width: 1024px;
         height: 768px;
+
+        border: solid .4px black;
+    }
+
+    .tablette {
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        margin: auto;
+        width: 1200px;
+        height: 900px;
+        border-radius: 30px;
+        border: solid .5px black;
+        background-color: white;
     }
 
 </style>
